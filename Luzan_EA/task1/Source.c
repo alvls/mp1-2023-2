3fbd665d1e9678fcc123d88eba3690868cded1c5
smallAbyss <eal1105@yandex.ru>
2023-09-27 00:05:08 +0300

    Update Source.c

--- Luzan_EA/task1/Source.c
+++ Luzan_EA/task1/Source.c
@@ -1,39 +1,37 @@
 #include <stdio.h>
 #include <math.h>
 #include <locale.h>
-// не дописана
+
 
 void main() {
+
 	setlocale(LC_ALL, "rus");
-	float r, R, p; // малый радиус, большой радиус, расстояние
-	float x1, x2, y1, y2;
+	float r, R, dist; // first radius, second radius, distance
+	float x1, x2, y1, y2; // coordinates
+	float e = 0.0001; // to check the equality float == float
+
 
 	scanf_s("%f %f %f", &x1, &y1, &R);
 	scanf_s("%f %f %f", &x2, &y2, &r);
 
-	//определение максимального радиуса
 
+	// determining the maximum radius, dist is used to avoid creating new variables
 	if (r > R) {
-		p = R;
+		dist = R;
 		R = r;
-		r = p;
+		r = dist;
 	}
 
-	p = sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
-	//printf_s("%f", p);
-	
 
+	dist = sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
 
 	
-	if ((R + r > p) && (R < p)) { printf_s("Окружности перескаются \n"); }
-	if (p > r + R) { printf_s("Никакая из окружностей не лежит внутри другой, не касаются, не пересекаются \n"); }
-	if (R == p + r) { printf_s("внутреннее касание \n"); }
-	if (R + r == p) { printf_s("внешнее касание \n"); }
-	if ((p + r < R) && (R > p)) { printf_s("одна окружность лежит в другой \n"); }
-	if ((p == 0) && (r != R)) { printf_s("совпали центры и не совпали радиусы \n"); }
-	if ((p == 0)&&(r == R)) { printf_s("полное совпадение \n"); }
+	if ((R + r > dist) && (R < dist)) { printf_s("окружности перескаются \n the circles intersect \n"); }
+	if (dist > r + R ) { printf_s("никакая из окружностей не лежит внутри другой, не касаются, не пересекаются \n none of the circles lies inside the other, do not touch, do not intersect \n"); }
+	if (R == dist + r) { printf_s("внутреннее касание \n inner touch \n"); }
+	if (R + r - dist < e) { printf_s("внешнее касание \n external touch \n"); }
+	if ((dist + r < R) && (R > dist)) { printf_s("одна окружность лежит в другой \n one circle lies in the other \n"); }
 	
-
 
 	system("pause");
 }
