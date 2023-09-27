cda30a325c7cd3f80b3d9d4c258360ce42e68c7a
4elodoy-molovek <ivandor228@outlook.com>
2023-09-27 17:22:24 +0300

    выполнить задание 1

---
+++ dorofeev_id/task1/task1.cpp
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <math.h>
+#include <locale.h>
+
+
+int main()
+{
+    setlocale (LC_ALL, "rus");
+    double PlDvp = (950*0.00001);
+    double PlDsp = (750*0.000001);
+    double PlWood = (700*0.000001);
+    double WDsp = 1.5;
+
+    int h, w, d;
+    while (1 == 1)
+    {
+        printf("Введите высоту\n");
+        scanf("%d", &h);
+        if (h >= 180 && h <= 220)
+        {
+            break;
+        }
+        else
+        {
+            printf("Некорректная высота\n");
+        }
+    }
+
+    while (1 == 1)
+    {
+        printf("Введите ширину\n");
+        scanf("%d", &w);
+        if (w >= 80 && w <= 120)
+        {
+            break;
+        }
+        else
+        {
+            printf("Некорректная ширина\n");
+        }
+    }
+
+    while (1 == 1)
+    {
+        printf("Введите глубину\n");
+        scanf("%d", &d);
+        if (d >= 50 && d <= 90)
+        {
+            break;
+        }
+        else
+        {
+            printf("Некорректная глубина\n");
+        }
+    }
+
+
+
+    double WeightSt = h*w*PlDvp;
+    double WeightBok = 2*(h*d*WDsp*PlDsp);
+    double WeightKr = 2*(w*d*WDsp*PlDsp);
+    double WeightDoor = 2*(h*(w/2)*1*PlWood);
+
+    double WPolka = w - 0.3;
+    int KolPolok = 0;
+    double HPolki = 40.15;
+    double hh = h;
+    while (hh - HPolki > 0)
+    {
+        KolPolok++;
+        hh -= HPolki;
+    }
+
+    double WeightPolok = KolPolok*(WPolka * WDsp * d * PlDsp);
+    double Weight = WeightBok + WeightDoor + WeightKr + WeightSt + WeightPolok;
+    printf("Вес шкафа %g кг\n", Weight);
+
+    return 0;
+}
+
+
