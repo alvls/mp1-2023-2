3e59493c8760c62615bcdb8448f7253299e196a3
Projectius_student <sprojectiustech@gmail.com>
2023-10-17 23:37:01 +0300

    v1

--- konstantinov_sa/task3/Source.c
+++ konstantinov_sa/task3/Source.c
@@ -5,43 +5,54 @@
 #include<time.h>
 #include<math.h>
 
-#define Test
+// | test mode with debug hints
+// V
+//#define Test
 
 //random in range
 int rir(int l, int r) {
 	return round(rand() * ((double)(r - l) / RAND_MAX) + l);
 }
 
-void gennum( int n, int places[5], int digits[10]) {
+int checkuniq(int n) {
+	int digits[10] = { 0 };
+	while (n > 0) {
+		int c = n % 10;
+		if (digits[c]){
+			return 0;
+		}	
+		digits[c] = 1;
+		n /= 10;
+	}
+	return 1;
+}
+
+void printnum(int n, int places[5]) {
+	for (int i = 0; i < n; i++) {
+		printf("%d", places[i]);
+	}
+	printf("\n");
+}
+
+void gennum(int n, int places[5], int digits[10]) {
 	places[0] = rir(1, 9);
-	digits[places[0]]=1;
+	digits[places[0]] = 1;
 	for (int i = 1; i < n; i++) {
 
 		while (1) {
-			int uniq = 1;
 			int e = rir(0, 9);
-			for (int k = 0; k < i; k++) {
-				//printf("try %d to [%d] check [%d] = %d\n", e, i, k, a[k]);
-				if ((places[k] == e)) {  // || ((i == 0) && (e == 0))
-					uniq = 0;
-					break;
-				}
-			}
-			if (uniq)
+			if (!digits[e])
 			{
 				places[i] = e;
 				digits[e] = 1;
 				break;
 			}
-
 		}
 	}
 
 #ifdef Test
-	for (int i = 0; i < n; i++) {
-		printf("%d", places[i]);
-	}
-	printf("\n");
+	printnum(n, places);
+	
 	for (int i = 0; i < 10; i++) {
 		printf("%d", i);
 	}
@@ -52,44 +63,59 @@ void gennum( int n, int places[5], int digits[10]) {
 #endif
 }
 
-
 int main() {
 	setlocale(LC_ALL, "");
-	srand(time(NULL));
 
-	int n = 3;
+	srand(time(NULL)* time(NULL));
+
+	int n = 0;
+	printf("Игра \"Быки и коровы\". Вам предстоит угадать загаданное компьютером число с различными цифрами.\nКомпьютер сообщает, сколько цифр угадано без совпадения с их позициями в загаданном числе (то есть количество коров)\nи сколько угадано вплоть до позиции в загаданном числе (то есть количество быков). Чтобы сдаться введите 0.\nВведите количество цифр числа, которое будет загадано:\n");
+	while (!((n>=2)&&(n<=5))) {
+		if (!scanf("%d", &n))
+			while (getchar() != '\n');
+	}
+
+	int mg = 1;
+	for (int i = 0; i < n;i++)
+		mg *= 10;
+
 	int num[5] = { -1,-1,-1,-1,-1 };
 	int digits[10] = { 0 };
-	
 	gennum(n, num, digits);
-	printf("\n");
-	
-	
-	int vp = 0;
+	printf("\n"); 
+
+	int vp = 0, tries = 0;
 	while(vp != n) {
 		vp = 0;
-		int g, vd = 0;
-		scanf("%d", &g);
+		int g = -1, vd = 0;
+
+		while ((!((mg/10 <= g) && (g< mg))) || (!checkuniq(g))) {
+			printf("      Введите %d-значное число с различными цифрами:\n", n);
+			if(!scanf("%d", &g))
+				while (getchar() != '\n');
+			if (g == 0) {
+				printf("Вы сдались, затратив %d попыток...\nЧисло было ", tries);
+				printnum(n, num);
+				system("pause");
+				return 0;
+			}
+		}
+		
+		tries += 1;
 		for (int i = n-1; i >= 0; i--) {
 			int c = g % 10;
 			g /= 10;
-			printf("c %d d%d n%d \n", c, digits[c], num[i]);
+			//printf("c%d d%d n%d \n", c, digits[c], num[i]);
 			if (digits[c])
 				vd++;
 			if (num[i] == c)
 				vp++;
 		}
-		printf("digits %d  positions %d\n", vd, vp);
+		printf("      Коров: %d  Быков: %d\n", vd-vp, vp);
 
 	}
 
-
-	//for (int i = 0; i < 10000;i++) {
-	//	int e = rir(0, 9);
-	//	//printf("%d", e);
-	//	if (e == 10) {
-	//		printf("\n!!!!\n");
-	//	}
-	//}
+	printf("Угадано за %d попыток\n", tries);
 	system("pause");
+	return 0;
 }
