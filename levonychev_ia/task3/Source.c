08aa0fc0951c02c5b6b8f78e509bd530a4037f01
fartekool <levonychev2006@gmail.com>
2023-10-24 23:15:12 +0300

    Добавил цвета в консоль

--- levonychev_ia/task3/Source.c
+++ levonychev_ia/task3/Source.c
@@ -1,10 +1,11 @@
-
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <locale.h>
 #include <time.h>
 #include <stdlib.h>
 #include <math.h>
+#include <Windows.h>
+#include <wincon.h>
 
 void game(int n);
 int exit_status(); // Если пользователь захочет сдаться
@@ -14,7 +15,8 @@ int len_num(int num, int n); // Возвращает 0, если длина num не совпадает с n, 1
 
 
 void main()
-{
+{   
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_GREEN);
     int n;
     setlocale(LC_ALL, "rus");
     srand(time(NULL));
@@ -22,12 +24,26 @@ void main()
     
     do
     {   
-        printf("\n\n\nВыберите n от 2 до 5. Введите 0, если хотите выйти.\n");
+        printf("\n\n\nВыберите n от ");
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED);
+        printf("2 ");
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_GREEN);
+        printf("до ");
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED);
+        printf("5 ");
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_GREEN);
+        printf(". Введите ");
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED);
+        printf("0 ");
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_GREEN);
+        printf("если хотите выйти.\n");
         do
-        {
+        {   
+            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED);
             scanf("%d", &n);
+            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_GREEN);
             if (!(n == 0 || (n >= 2 && n <= 5)))
-                printf("Повторите попытку.\n");
+                printf("Некорректный ввод.\n");
         } while (!(n == 0 || (n >= 2 && n <=5)));
         
         if (n >= 2 && n <= 5)
@@ -55,14 +71,17 @@ void game(int n)
         num = pow(10, n-1) + (rand() % a);
     } while (f(num, n) == 0);
     // Генерация числа
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED);
+    printf("\n--------------------\n");
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_GREEN);
     if (n == 2)
-        printf("\n--------------------\nВы выбрали режим с двузначным числом.\n");
+        printf("Вы выбрали режим с двузначным числом.\n");
     if (n == 3)
-        printf("\n--------------------\nВы выбрали режим с трехзначным числом.\n");
+        printf("Вы выбрали режим с трехзначным числом.\n");
     if (n == 4)
-        printf("\n--------------------\nВы выбрали режим с четырехзначным числом.\n");
+        printf("Вы выбрали режим с четырехзначным числом.\n");
     if (n == 5)
-        printf("\n--------------------\nВы выбрали режим с пятизначным числом.\n");
+        printf("Вы выбрали режим с пятизначным числом.\n");
     printf("Пытайтесь угадать число. Программа ожидает число с неповторяющимися цифрами. Если вы введете с повторяющимися, она попросит ввод заново. Вы всегда можете сдаться, введя 0.\n");
     printf("%d\n", num); // показать загаданное число
     do
@@ -71,7 +90,9 @@ void game(int n)
         do
         {   
             printf("   ");
+            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED);
             scanf("%d", &user_num);
+            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_GREEN);
             if (f(user_num, n) == 0 || len_num(user_num, n) == 0)
                 printf("   Некорректный ввод.\n");
         } while (f(user_num, n) == 0 || len_num(user_num, n) == 0);
@@ -83,8 +104,12 @@ void game(int n)
 
             flag_exit = exit_status();
             if (flag_exit == 1)
-            {
-                printf("Загаданное число: %d\n--------------------\n", num);
+            {   
+                printf("Загаданное число: ");
+                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED);
+                printf("%d", num);
+                printf("\n--------------------\n", num);
+                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_GREEN);
                 return;
             }
             if (flag_exit == 0)
@@ -94,13 +119,26 @@ void game(int n)
         {  
             cows = cows_bulls(num, user_num, 0);
             bulls = cows_bulls(num, user_num, 1);
-            printf("   Количество коров: %d\n   Количество быков: %d\n", cows, bulls);
+            printf("   Количество коров: ");
+            if (cows > 0)
+                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED);
+            printf("%d\n", cows);
+            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_GREEN);
+            printf("   Количество быков: ");
+            if (bulls > 0)
+                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED);
+            printf("%d\n", bulls);
+            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_GREEN);
         }
 
         i++;
     } while (bulls != n);
 
-    printf("Вы угадали! Это число: %d\n--------------------\n", num);
+    printf("Вы угадали! Это число: ");
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED);
+    printf("%d", num);
+    printf("\n--------------------\n", num);
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_GREEN);
 
     return;
 }
@@ -109,9 +147,13 @@ int exit_status()
 {   
     int flag;
     do
-    {   
+    {      
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED);
         printf("Вы точно хотите сдаться? 0 - НЕТ, 1 - ДА: ");
         scanf("%d", &flag);
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_GREEN);
+        if ((flag != 1) && (flag != 0))
+            printf("Некорректный ввод.\n");
     } while ((flag != 1) && (flag != 0));
     
     return flag;
