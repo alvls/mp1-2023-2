d6862e6a1d65421d8f916ad2833a1b202c0eff0f
Ilya Baykov <snowdog289@gmail.com>
2023-10-25 19:55:26 +0300

    corrected task 2

--- Baikov_ID/task3/Source.c
+++ Baikov_ID/task3/Source.c
@@ -8,36 +8,61 @@
 int main() {
 
 	setlocale(LC_ALL, "rus");
-
 	int num[5] = { 0 };
 	int g[] = { 0 };
 	int len, guess, bulls = 0, cows = 0;
+	
+	printf("Какой длины должно быть число? (от 2 до 5)\n");
+	
+	scanf("%d", &len);
+	
 
 	srand(time(NULL));
+	if (len == 4 || len == 5) {
+		do
+		{
+			num[0] = 1 + rand() % 9;
 
-	printf("Какой длины должно быть число? (от 2 до 5)\n");
-	scanf("%d", &len);
+			for (int i = 1; i < len; i++) {
+				num[i] = rand() % 10;
+			}
+		} while (num[0] == num[1] || num[0] == num[2] || num[0] == num[3] ||
+			num[0] == num[4] || num[1] == num[2] || num[1] == num[3] ||
+			num[1] == num[4] || num[2] == num[3] || num[2] == num[4] ||
+			num[3] == num[4]);
+	}
+	else if (len == 2){
+		do
+		{
+			num[0] = 1 + rand() % 9;
+			num[1] = rand() % 10;
+			
+		} while (num[0] == num[1]);
+	}
+	else if (len == 3) {
+		do
+		{
+			num[0] = 1 + rand() % 9;
+			num[1] = rand() % 10;
+			num[2] = rand() % 10;
 
-	do
-	{
-		num[0] = 1 + rand() % 9;
 
-		for (int i = 1; i < len; i++) {
-			num[i] = rand() % 10;
-		}
-	} while (num[0] == num[1] || num[0] == num[2] || num[0] == num[3] ||
-		num[0] == num[4] || num[1] == num[2] || num[1] == num[3] ||
-		num[1] == num[4] || num[2] == num[3] || num[2] == num[4] ||
-		num[3] == num[4]);
+		} while (num[0] == num[1] || num[0] == num[2] || num[1] == num[2]);
+	}
+	else {
+		printf("Неверное число");
+	}
+	
 	//the number has been got
+	
 	do
 	{
 		printf("\nПостарайтесь угадать число из %d цифр\n", len);
 		scanf("%d", &guess);
-		
+
 		cows = 0;
 		bulls = 0;
-		
+
 		for (int i = len - 1; i > -1; i--) {
 
 			g[i] = guess % 10;
