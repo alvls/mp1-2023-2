641187248d4ea75a875d44b4dc345ac7b74bfec1
Акимов Илья <akimov.ilya2018@gmail.com>
2023-11-08 22:30:21 +0300

    Исправил ошибки в task 4

--- akimov.ia/task4/Source.c
+++ akimov.ia/task4/Source.c
@@ -6,55 +6,18 @@
 #include <time.h>
 #include <stdlib.h>
 
-void ProductName(int a)
+char ProductName(int a)// a - номер нужного нам товара в массиве(от 0 до 9)
 {
-	if (a == 0)
-		printf("GeForce RTX 3060\n");
-	else if (a == 1)
-		printf("GeForce GTX 1660\n");
-	else if (a == 2)
-		printf("GeForce RTX 4060\n");
-	else if (a == 3)
-		printf("GeForce GTX 1650\n");
-	else if (a == 4)
-		printf("AMD Radeon RX 6600\n");
-	else if (a == 5)
-		printf("GeForce RTX 3070 Ti\n");
-	else if (a == 6)
-		printf("GeForce RTX 4090\n");
-	else if (a == 7)
-		printf("GeForce RTX 4070 Ti\n");
-	else if (a == 8)
-		printf("AMD Radeon RX 6700\n");
-	else if (a == 9)
-		printf("GeForce RTX 4070\n");
+	char arr[][20] = { "GeForce RTX 3060", "GeForce GTX 1660", "GeForce RTX 4060", "GeForce GTX 1650", "AMD Radeon RX 6600",
+		"GeForce RTX 3070 Ti", "GeForce RTX 4090", "GeForce RTX 4070 Ti", "AMD Radeon RX 6700", "GeForce RTX 4070" };
+	return puts(arr[a]);
 
 }
 
-int ProductPrice(int a)
+int ProductPrice(int b)// b номер нужной нам цены в массиве(от 0 до 9)
 {
-	int price;
-	if (a == 0)
-		price = 34000;
-	else if (a == 1)
-		price = 26000;
-	else if (a == 2)
-		price = 40000;
-	else if (a == 3)
-		price = 16000;
-	else if (a == 4)
-		price = 27000;
-	else if (a == 5)
-		price = 62000;
-	else if (a == 6)
-		price = 184000;
-	else if (a == 7)
-		price = 96000;
-	else if (a == 8)
-		price = 45000;
-	else if (a == 9)
-		price = 78000;
-	return price;
+	int price[10] = { 34000, 26000, 40000, 16000, 27000, 62000, 184000, 96000, 45000, 78000 };
+	return price[b];
 }
 
 float Discount()
@@ -73,9 +36,11 @@ void main()
 {
 	setlocale(LC_ALL, "rus");
 	srand(time(NULL));
-	int barcode, flag, choice, discount1, total_price_without_discount = 0, total_price_with_discount = 0, total_discount = 0, i = 0;
+	int flag, choice, discount1, total_price_without_discount = 0, total_price_with_discount = 0, total_discount = 0, i = 0, barcodeint;
 	float discount;
 	int quantity[10] = { 0 };
+	char barcode[4] = { 0 };
+	char barcodestr[4] = "";
 	printf("Добро пожаловать в магазин видеокарт! У нас есть на выбор 10 видеокарт с указанием их названий, цен в рублях и штрих-кодов"
 		" соответсвенно:\n");
 	printf("1.GeForce RTX 3060 - 34000 рублей/шт - 2735\n");
@@ -92,68 +57,60 @@ void main()
 	printf("Скидки устанавливаются на каждый товар независимо от 5 до 50 процентов.\n\n");
 	do
 	{
-		do
-		{
-			flag = 0;
-			printf("Введите штрих-код: ");
-			scanf("%d", &barcode);
-			while (getchar() != '\n');
-			if (barcode != 2735 && barcode != 5962 && barcode != 3933 && barcode != 9087 && barcode != 4352
-				&& barcode != 4727 && barcode != 2821 && barcode != 6327 && barcode != 4341 && barcode != 3520)
-			{
-				flag = 1;
-				printf("Вы ввели неправильный штрих-код!\n");
-			}
-		} while (flag);
-		if (barcode == 2735)
+		printf("Введите штрих-код: ");
+		scanf("%4s", barcodestr);
+		for (i = 0; i < 4; i++)
+			barcode[i] = barcodestr[i];
+		barcodeint = strtol(barcodestr, NULL, 10);
+		if (barcodeint == 2735)
 		{
 			quantity[0]++;
-			printf("Вы отсканировали %d - GeForce RTX 3060 - %d рублей/шт\n", barcode, ProductPrice(0));
+			printf("Вы отсканировали %d - GeForce RTX 3060 - %d рублей/шт\n", barcodeint, ProductPrice(0));
 		}
-		else if (barcode == 5962)
+		else if (barcodeint == 5962)
 		{
 			quantity[1]++;
-			printf("Вы отсканировали %d - GeForce GTX 1660 - %d рублей/шт\n", barcode, ProductPrice(1));
+			printf("Вы отсканировали %d - GeForce GTX 1660 - %d рублей/шт\n", barcodeint, ProductPrice(1));
 		}
-		else if (barcode == 3933)
+		else if (barcodeint == 3933)
 		{
 			quantity[2]++;
-			printf("Вы отсканировали %d - GeForce RTX 4060 - %d рублей/шт\n", barcode, ProductPrice(2));
+			printf("Вы отсканировали %d - GeForce RTX 4060 - %d рублей/шт\n", barcodeint, ProductPrice(2));
 		}
-		else if (barcode == 9087)
+		else if (barcodeint == 9087)
 		{
 			quantity[3]++;
-			printf("Вы отсканировали %d - GeForce GTX 1650 - %d рублей/шт\n", barcode, ProductPrice(3));
+			printf("Вы отсканировали %d - GeForce GTX 1650 - %d рублей/шт\n", barcodeint, ProductPrice(3));
 		}
-		else if (barcode == 4352)
+		else if (barcodeint == 4352)
 		{
 			quantity[4]++;
-			printf("Вы отсканировали %d - AMD Radeon RX 6600 - %d рублей/шт\n", barcode, ProductPrice(4));
+			printf("Вы отсканировали %d - AMD Radeon RX 6600 - %d рублей/шт\n", barcodeint, ProductPrice(4));
 		}
-		else if (barcode == 4727)
+		else if (barcodeint == 4727)
 		{
 			quantity[5]++;
-			printf("Вы отсканировали %d - GeForce RTX 3070 Ti - %d рублей/шт\n", barcode, ProductPrice(5));
+			printf("Вы отсканировали %d - GeForce RTX 3070 Ti - %d рублей/шт\n", barcodeint, ProductPrice(5));
 		}
-		else if (barcode == 2821)
+		else if (barcodeint == 2821)
 		{
 			quantity[6]++;
-			printf("Вы отсканировали %d - GeForce RTX 4090 - %d рублей/шт\n", barcode, ProductPrice(6));
+			printf("Вы отсканировали %d - GeForce RTX 4090 - %d рублей/шт\n", barcodeint, ProductPrice(6));
 		}
-		else if (barcode == 6327)
+		else if (barcodeint == 6327)
 		{
 			quantity[7]++;
-			printf("Вы отсканировали %d - GeForce RTX 4070 Ti - %d рублей/шт\n", barcode, ProductPrice(7));
+			printf("Вы отсканировали %d - GeForce RTX 4070 Ti - %d рублей/шт\n", barcodeint, ProductPrice(7));
 		}
-		else if (barcode == 4341)
+		else if (barcodeint == 4341)
 		{
 			quantity[8]++;
-			printf("Вы отсканировали %d - AMD Radeon RX 6700 - %d рублей/шт\n", barcode, ProductPrice(8));
+			printf("Вы отсканировали %d - AMD Radeon RX 6700 - %d рублей/шт\n", barcodeint, ProductPrice(8));
 		}
-		else if (barcode == 3520)
+		else if (barcodeint == 3520)
 		{
 			quantity[9]++;
-			printf("Вы отсканировали %d - GeForce RTX 4070 - %d рублей/шт\n", barcode, ProductPrice(9));
+			printf("Вы отсканировали %d - GeForce RTX 4070 - %d рублей/шт\n", barcodeint, ProductPrice(9));
 		}
 		do
 		{
@@ -166,6 +123,7 @@ void main()
 		} while (flag);
 	} while (choice);
 	printf("Покупки завершены!\n\n");
+	i = 0;
 	do
 	{
 		if (quantity[i] != 0)
