8b0fdec90f0b6435248b04a7baa1b9847ea1be21
LeoGNZV <leogonz2005@mail.ru>
2023-11-13 10:53:00 +0300

    Обновлено Задание №4

--- gonozov_la/task4/source.c
+++ gonozov_la/task4/source.c
@@ -4,25 +4,14 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
-struct p2354 { //наименование, стоимость за единицу товара, скидка
-	char name[100];
-	int price;
-	double discount;
-};
-struct p3576 { //наименование, стоимость за единицу товара, скидка
-	char name[100];
-	int price;
-	double discount;
-};
-struct p8903 { //наименование, стоимость за единицу товара, скидка
+struct shop { //наименование, стоимость за единицу товара, скидка
 	char name[100];
 	int price;
 	double discount;
 };
 void main() {
-	struct p2354 t2354 = { "Молоко,1л", 70, 0.15 }; int c2354 = 0;
-	struct p3576 t3576 = { "Рис, 500г", 120, 0.2 }; int c3576 = 0;
-	struct p8903 t8903 = { "Зелёный чай \"Счастье художника\"", 100, 0.1 }; int c8903 = 0;
+	struct shop product[] = {{"Молоко,1л", 70, 0.15}, {"Рис, 500г", 120, 0.2}, {"Зелёный чай \"Счастье художника\"", 100, 0.1}};
+	int c2354 = 0, c3576 = 0, c8903 = 0;
 	setlocale(LC_ALL, "rus");
 	int action;
 	int barcode[5] = { -1, -1, -1, -1 };
@@ -69,13 +58,13 @@ void main() {
 				printf("Сначала вы должны отсканировать товар");
 			}
 			else if (number == 2354) {
-				printf("%s, Стоимость: %d руб, Скидка: %.0lf%%", t2354.name, t2354.price, t2354.discount*100);
+				printf("%s, Стоимость: %d руб, Скидка: %.0lf%%", product[0].name, product[0].price, product[0].discount * 100);
 			}
 			else if (number == 3576) {
-				printf("%s, Стоимость: %d руб, Скидка: %.0lf%%", t3576.name, t3576.price, t3576.discount * 100);
+				printf("%s, Стоимость: %d руб, Скидка: %.0lf%%", product[1].name, product[1].price, product[1].discount * 100);
 			}
 			else if (number == 8903) {
-				printf("%s, Стоимость: %d руб, Скидка: %.0lf%%", t8903.name, t8903.price, t8903.discount * 100);
+				printf("%s, Стоимость: %d руб, Скидка: %.0lf%%", product[2].name, product[2].price, product[2].discount * 100);
 			}
 		}
 		if (action == 3) {
@@ -85,20 +74,20 @@ void main() {
 			else if (number == 2354) {
 				c2354++;
 				printf("Товар добавлен в чек");
-				total_cost += t2354.price;
-				total_disc += t2354.price * t2354.discount;
+				total_cost += product[0].price;
+				total_disc += product[0].price * product[0].discount;
 			}
 			else if (number == 3576) {
 				c3576++;
 				printf("Товар добавлен в чек");
-				total_cost += t3576.price;
-				total_disc += t3576.price * t3576.discount;
+				total_cost += product[1].price;
+				total_disc += product[1].price * product[1].discount;
 			}
 			else if (number == 8903) {
 				c8903++;
 				printf("Товар добавлен в чек");
-				total_cost += t8903.price;
-				total_disc += t8903.price * t8903.discount;
+				total_cost += product[2].price;
+				total_disc += product[2].price * product[2].discount;
 
 			}
 		}
@@ -106,13 +95,13 @@ void main() {
 			if (total_cost != 0) {
 				printf("Ваш чек:");
 				if (c2354 != 0) {
-					printf("%s, Стоимость за единицу: %d, Количество: %d,\nОбщая стоимость за товар: %lf\n", t2354.name, t2354.price, c2354, (double)c2354 * ((double)t2354.price - (double)t2354.price * t2354.discount));
+					printf("%s, Стоимость за единицу: %d, Количество: %d,\nОбщая стоимость за товар: %lf\n", product[0].name, product[0].price, c2354, (double)c2354 * ((double)product[0].price - (double)product[0].price * product[0].discount));
 				}
 				if (c3576 != 0) {
-					printf("%s, Стоимость за единицу: %d, Количество: %d,\nОбщая стоимость за товар: %lf\n", t3576.name, t3576.price, c3576, (double)c3576 * ((double)t3576.price - (double)t3576.price * t3576.discount));
+					printf("%s, Стоимость за единицу: %d, Количество: %d,\nОбщая стоимость за товар: %lf\n", product[1].name, product[1].price, c3576, (double)c3576 * ((double)product[1].price - (double)product[1].price * product[1].discount));
 				}
 				if (c8903 != 0) {
-					printf("%s, Стоимость за единицу: %d, Количество: %d,\nОбщая стоимость за товар: %lf\n", t8903.name, t8903.price, c8903, (double)c8903 * ((double)t8903.price - (double)t8903.price * t8903.discount));
+					printf("%s, Стоимость за единицу: %d, Количество: %d,\nОбщая стоимость за товар: %lf\n", product[2].name, product[2].price, c8903, (double)c8903 * ((double)product[2].price - (double)product[2].price * product[2].discount));
 				}
 				printf("Общая стоимость товаров в покупке: %d, Cуммарная скидка: %lf, Итоговая сумма к оплате: %lf", total_cost, total_disc, (double)total_cost - total_disc);
 			}
@@ -123,10 +112,10 @@ void main() {
 		}
 		if (action == 5)
 			if (total_cost != 0)
-				printf("Итоговая сумма к оплате: %.0lf", (float)total_cost - total_disc);
+				printf("Итоговая сумма к оплате: %.1lf", (float)total_cost - total_disc);
 			else
 				printf("Вы ещё не добавили ни одного товара");
-
+		printf("\n");
 		scanf("%d", &action);
 		while (!(1 <= action && action <= 6)) {
 			printf("Некорректный ввод, введите число в промежутке от 1 до 6 включительно: ");
