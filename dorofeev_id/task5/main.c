247dd6a0165fa7c7f1c70422c6b878e58a685679
4elodoy-Molovek <87127831+4elodoy-Molovek@users.noreply.github.com>
2023-11-26 21:15:54 +0300

    Update main.c

--- dorofeev_id/task5/main.c
+++ dorofeev_id/task5/main.c
@@ -2,21 +2,50 @@
 #include <stdlib.h>
 #include <string.h>
 #include "sorts.h"
-#include "files.h"
 #include <locale.h>
 
 
 
 int main()
 {
-	setlocale(LC_ALL, "rus");
-	char path[] = "D:\\Desktop\\ONB2\\*.*";
-	int file_count;
-	file* lol = get_files(path, &file_count);
-	print_files(lol, file_count);
-	printf("\n%d", file_count);
-	printf("\n\n\ncomplete");
-	return 0;
+    setlocale(LC_ALL, "rus");
+    char path[] = "D:\\VS\\source\\repos\\mp1-2023-2\\mp1-2023-2\\dorofeev_id\\task5\\*.*";
+    int file_count;
+    file* lol = get_files(path, &file_count);
+
+    if (lol == NULL) {
+        // Handle memory allocation failure
+        printf("Memory allocation failed!\n");
+        return 1;
+    }
+
+    print_files(lol, file_count);
+    printf("\n%d", file_count);
+    printf("\n\n\ncomplete");
+
+    bubble_size(lol, file_count, 1);
+    print_files(lol, file_count);
+
+    selection_size(lol, file_count, 1);
+    print_files(lol, file_count);
+
+    insertion_size(lol, file_count, 1);
+    print_files(lol, file_count);
+
+    merge_sort(lol, 0, file_count - 1, 1);
+    print_files(lol, file_count);
+
+    shell_size(lol, file_count, 1);
+    print_files(lol, file_count);
+
+    counting_size(lol, file_count, 1);
+    print_files(lol, file_count);
+
+    hoar_size(lol, file_count, 1);
+    print_files(lol, file_count);
+
+    free(lol);
+
+    return 0;
 }
 
-//дальше - больше
