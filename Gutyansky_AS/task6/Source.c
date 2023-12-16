dc0d95f406a992581393a1548e421a8e7b305aac
Alexey Gutyansky <75169888+AlexixRugis@users.noreply.github.com>
2023-12-16 14:18:24 +0300

    arcsin arccos added

--- Gutyansky_AS/task6/Source.c
+++ Gutyansky_AS/task6/Source.c
@@ -14,7 +14,9 @@ int main() {
 	double ex = taylor_exp(x, 100);
 	double si = taylor_sin(x, 100);
 	double co = taylor_cos(x, 100);
-	printf("%lf %lf %lf", ex, si, co);
+	double asi = taylor_arcsin(x, 1000);
+	double aco = taylor_arccos(x, 1000);
+	printf("%lf %lf %lf %lf %lf", ex, si, co, asi, aco);
 
 	system("pause");
 	return 0;
--- Gutyansky_AS/task6/tailor_math.c
+++ Gutyansky_AS/task6/tailor_math.c
@@ -1,5 +1,7 @@
 #include "taylor_math.h"
 
+#define PI 3.14159265358979323846
+
 double taylor_exp(double x, unsigned int members_count) {
 	double res;
 	double member;
@@ -52,14 +54,22 @@ double taylor_cos(double x, unsigned int members_count) {
 }
 
 double taylor_arcsin(double x, unsigned int members_count) {
-	// TODO:!
-}
-
-double taylor_arccos(double x, unsigned int members_count) {
 	double res;
 	double member;
 	unsigned int i;
 
 	res = 0.0;
-	member = 
+	member = x;
+
+	for (i = 1; i <= members_count; i++) {
+		res += member;
+
+		member *= (x * x) * (i * i) / ((i + 1) * (i + 2));
+	}
+
+	return res;
+}
+
+double taylor_arccos(double x, unsigned int members_count) {
+	return (PI * 0.5) - taylor_arcsin(x, members_count);
 }
