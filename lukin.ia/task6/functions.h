5d6b19acd5da96846f59f42e7e98dc8ed58448a9
elus10n <dizbvalkyre@gmail.com>
2023-12-18 17:42:16 +0300

    task 6

--- lukin.ia/task6/functions.h
+++ lukin.ia/task6/functions.h
@@ -10,7 +10,7 @@ double sin_x(double x, int count)
 	int flag, k=3, j=5, f_k, f_j;
 	if (count == 1)
 		return x;
-	flag = 1;
+	flag = 1;//механизм чередования
 	for (int i = 2; i <= count; i++)
 	{
 		tmp = 1;
@@ -44,7 +44,7 @@ double cos_x(double x, int count)
 	int flag, k = 2, j = 4, f_k, f_j;
 	if (count == 1)
 		return 1;
-	flag = 1;
+	flag = 1;//механизм чередования знаков
 	for (int i = 2; i <= count; i++)
 	{
 		tmp = 1;
@@ -83,9 +83,38 @@ double expan(double x, int count)
 	return e;
 }
 
-double cosec(int x)
+double cosec(double x, int count)
 {
-
+	double e = x, tmp;
+	int flag, k = 3, j = 5, f_k, f_j;
+	if (count == 1)
+		return x;
+	flag = 1;//механизм чередования
+	for (int i = 2; i <= count; i++)
+	{
+		tmp = 1;
+		if (flag == 1)//отриц
+		{
+			for (f_k = 1; f_k <= k; f_k++)
+			{
+				tmp = tmp * x / f_k;
+			}
+			e = e - tmp;
+			k = k + 4;
+			flag = 2;
+		}
+		else if (flag == 2)//полож
+		{
+			for (f_j = 1; f_j <= j; f_j++)
+			{
+				tmp = tmp * x / f_j;
+			}
+			e = e + tmp;
+			j = j + 4;
+			flag = 1;
+		}
+	}
+	return 1/e;
 }
 
 void func_arr()
@@ -96,4 +125,6 @@ void func_arr()
 	etalon[1] = sin;
 	function[2] = cos_x;
 	etalon[2] = cos;
+	function[4] = cosec;
+	etalon[4] = sin;
 }
